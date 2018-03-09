8f552560815f4fcb268e905e9e1fff6b9a9dabd5
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-03-09 12:19:52 -0800

    very basic styling

--- public/index.js
+++ public/index.js
@@ -1,19 +1,37 @@
-const obj = {
-	data: 'stuff',
-	otherData: 'other stuff'
+
+const MOCK_WORD_DATA = {
+	"word": "abate",
+	"definition": "(of something perceived as hostile, threatening, or negative) become less intense or widespread."
+}
+
+const MOCK_USER_DATA = {
+	"username": "email@email.com",
+	"password": "password",
+	"words": [
+		{
+			"lemma": "abate",
+			"definition": "(of something perceived as hostile, threatening, or negative) become less intense or widespread.",
+			"exposures": 12
+		},
+		{
+			"lemma": "variegate",
+			"definition": "to diversify in external appearance especially with different colors",
+			"exposures": 5
+		}
+	]
 }
 
 function handleSubmit() {
-	$('.text-to-count').on('submit', function(event) {
+	$('.words-and-text').on('submit', function(event) {
 		event.preventDefault()
 
 		$.ajax('/broken-up-text', {
 			method: 'POST',
-			data: obj
+			data: MOCK_WORD_DATA
 		})
 		.then(res => {
 			console.log(res)
-			$('.broken-up-text').html(res.hello)
+			$('.word-lists').html(res.hello)
 		})
 	})
 }
