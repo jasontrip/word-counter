4818b409761254283e0c975ccc31684720ba08b1
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-03-26 12:12:23 -0700

    remove port fallback

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = 
 	process.env.DATABASE_URL ||
 	'mongodb://localhost/words-app'
-exports.PORT = process.env.port || 8080
+exports.PORT = process.env.port
 exports.JWT_SECRET = process.env.JWT_SECRET
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
 exports.TEST_DATABASE_URL = 'mongodb://localhost/test-words-app'
