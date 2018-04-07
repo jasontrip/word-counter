60b270506bb9a3c4b51b4a601200acd077a2d4cf
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-04-06 17:41:30 -0700

    fix User.create back to not adding searchWordList

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -21,7 +21,7 @@ exports.addUser = (req, res) => {
 	if (invalid) return res.status(422).json(invalid)
 
 	// create user if username is unique
-	let {username, password, firstName='', lastName='', addWords=[]} = req.body
+	let {username, password, firstName='', lastName=''} = req.body
 	firstName = firstName.trim()
 	lastName = lastName.trim()
 
@@ -43,8 +43,7 @@ exports.addUser = (req, res) => {
 				username,
 				password: hash,
 				firstName,
-				lastName,
-				addWords
+				lastName
 			})
 		})
 		.then(user => {
