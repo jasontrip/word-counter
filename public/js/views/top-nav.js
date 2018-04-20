ce363397285eb492ad81bb544442d16f8ca5c4f8
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-04-19 17:39:19 -0700

    add word count to top nav

--- public/js/views/top-nav.js
+++ public/js/views/top-nav.js
@@ -3,7 +3,10 @@ const topNav = (() => {
 	function render() {
 		const html = `
 			<div class="hamburger"><i class="material-icons hamburger-icon">menu</i></div>
-			<div class="current-view">English > Oxford Dictionary</div>
+			<div class="current-view">
+				${store.user.wordList.length}
+				word${(store.user.wordList.length > 1)?'s':''}
+			</div>
 			<div class="log-out-hover">
 					<button class="js-log-out">Logout</button>
 			</div>
