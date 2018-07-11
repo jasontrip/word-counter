3750eacf985d865f3dccb10538e353580bda72be
jasont@gmail.com <jason@Jasons-MacBook-Pro.local>
2018-07-11 12:13:28 -0700

    fix width and word pluralization

--- public/js/views/top-nav.js
+++ public/js/views/top-nav.js
@@ -1,11 +1,19 @@
 const topNav = (() => {
 
 	function render() {
+
+		let wordCount = ''
+		if (store.user.wordList.length > 0) {
+			wordCount = `
+				${store.user.wordList.length}
+				word${(store.user.wordList.length > 1)?'s':''}
+			`
+		}
+
 		const html = `
 			<div class="hamburger"><i class="material-icons hamburger-icon">menu</i></div>
 			<div class="current-view">
-				${store.user.wordList.length}
-				word${(store.user.wordList.length > 1)?'s':''}
+				${wordCount}
 			</div>
 			<div class="log-out-hover">
 					<button class="js-log-out">Logout</button>
